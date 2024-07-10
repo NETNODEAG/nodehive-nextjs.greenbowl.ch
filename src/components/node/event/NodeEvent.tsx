0b09705f3282ae146f6a248e4ecf17ce295c6e0d
Natalia Justicia <nataliajusticia@gmail.com>
2024-07-10 13:51:00 +0200

    Styling NodeEvent component

--- src/components/node/event/NodeEvent.tsx
+++ src/components/node/event/NodeEvent.tsx
@@ -1,16 +1,66 @@
-import { DrupalNode } from '@/nodehive/types';
+import Image from 'next/image';
+import { Locale } from '@/nodehive/i18n-config';
+import { DrupalNode, DrupalParagraph } from '@/nodehive/types';
+import { format } from 'date-fns';
+
+import Paragraph from '@/components/paragraph/Paragraph';
 
 export interface NodeEventProps {
   node: DrupalNode;
+  lang: Locale;
 }
 
-export default function NodeEvent({ node }: NodeEventProps) {
+export default function NodeEvent({ node, lang }: NodeEventProps) {
   const nodeData = node?.data;
+
   const title = nodeData?.title;
+  const location = nodeData?.field_event_location;
+  const date = nodeData?.field_event_date;
+  const headerImage = nodeData?.field_header_image;
+  const image = headerImage?.field_media_image;
+  const imageUrl = image?.image_style_uri?.wide;
+  const imageAlt = image?.meta?.alt;
+  const paragraphs = nodeData?.field_paragraphs;
 
   return (
     <article data-node-type="Event">
-      <h1>{title}</h1>
+      <div className="relative flex h-dvh max-h-[810px] items-center justify-center">
+        {title && (
+          <div className="container-wrapper flex items-center justify-center">
+            <div className="z-10 flex flex-col items-center justify-center">
+              <p className="byline mb-3 text-white">
+                {format(date, 'd MMMM yyyy, HH:SS')}
+              </p>
+
+              <h1 className="text-center text-white">{title}</h1>
+
+              <p className="mt-6 font-unbounded text-lg font-semibold uppercase tracking-wider text-primary">
+                {location}
+              </p>
+            </div>
+          </div>
+        )}
+
+        <Image
+          src={imageUrl}
+          alt={imageAlt}
+          width={1920}
+          height={1080}
+          className="absolute inset-0 h-full w-full object-cover"
+        />
+
+        <div className="absolute inset-0 bg-black opacity-50" />
+      </div>
+
+      {Array.isArray(paragraphs) && (
+        <>
+          {paragraphs?.map((paragraph: DrupalParagraph) => {
+            return (
+              <Paragraph key={paragraph.id} paragraph={paragraph} lang={lang} />
+            );
+          })}
+        </>
+      )}
     </article>
   );
 }
--- src/nodehive/jsonapi-config.ts
+++ src/nodehive/jsonapi-config.ts
@@ -8,10 +8,15 @@ export const NodeHiveConfig = {
         'field_paragraphs.field_media_ref_media',
         'field_paragraphs.field_media_ref_media.field_media_image',
       ],
-      'media-image': {
-        addFilter: [['status', '1']],
-        addInclude: ['field_media_image'],
-      },
+    },
+    'node-event': {
+      addInclude: [
+        'field_header_image',
+        'field_header_image.field_media_image',
+        'field_paragraphs',
+        'field_paragraphs.field_media_ref_media',
+        'field_paragraphs.field_media_ref_media.field_media_image',
+      ],
     },
   },
 };
