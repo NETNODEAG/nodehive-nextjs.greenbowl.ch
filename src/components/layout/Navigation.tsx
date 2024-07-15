105ee939ef0831ebb65bc526f7b4e4bb16431194
Natalia Justicia <nataliajusticia@gmail.com>
2024-07-15 10:32:24 +0200

    chore: Update npm dependencies for @radix-ui/react-dropdown-menu and @radix-ui/react-menubar

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,6 +1,31 @@
 import Link from 'next/link';
 import { createServerClient } from '@/nodehive/client';
 
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from '@/components/ui/navigation-menu';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarShortcut,
+  MenubarTrigger,
+} from '../ui/menubar';
+
 interface NavigationProps {
   menuId: string;
 }
@@ -14,20 +39,73 @@ export default async function Navigation({ menuId }: NavigationProps) {
     return null;
   }
 
+  const mainNavigation = Object.values(
+    navigation?.data?.reduce((acc, item) => {
+      if (!item.parent) {
+        // This is a parent menu item
+        acc[item.id] = { ...item, subMenu: [] };
+      } else if (acc[item.parent]) {
+        // This is a submenu item for an existing parent
+        acc[item.parent].subMenu.push(item);
+      }
+      return acc;
+    }, {})
+  );
+
   return (
     <nav className="hidden md:block">
-      <ul className="flex gap-8">
-        {navigation?.data?.map((item) => (
-          <li key={item.id}>
-            <Link
-              href={item.url}
-              className="text-lg font-semibold uppercase tracking-wider"
-            >
-              {item.title}
-            </Link>
-          </li>
+      <Menubar className="bg-transparent">
+        {mainNavigation?.map((item: any) => (
+          <MenubarMenu key={item?.id}>
+            {item.subMenu.length > 0 ? (
+              <MenubarTrigger>{item?.title}</MenubarTrigger>
+            ) : (
+              <Link href={item.url}>{item?.title}</Link>
+            )}
+
+            {item.subMenu.length > 0 && (
+              <MenubarContent>
+                {item.subMenu.map((subItem: any) => (
+                  <MenubarItem key={subItem.id}>
+                    <Link href={subItem.url}>{subItem.title}</Link>
+                  </MenubarItem>
+                ))}
+              </MenubarContent>
+            )}
+          </MenubarMenu>
         ))}
-      </ul>
+      </Menubar>
+      {/* 
+      <NavigationMenu>
+        <NavigationMenuList className="gap-4">
+          {mainNavigation?.map((item: any) => (
+            <NavigationMenuItem key={item?.id}>
+              {item.subMenu.length > 0 ? (
+                <>
+                  <NavigationMenuTrigger className="relative bg-transparent p-0 text-lg font-normal">
+                    {item?.title}
+                  </NavigationMenuTrigger>
+
+                  <NavigationMenuContent>
+                    <ul className="grid w-full gap-2 p-2 text-base">
+                      {item.subMenu.map((subItem: any) => (
+                        <NavigationMenuLink
+                          key={subItem.id}
+                          className="text-normal"
+                        >
+                          <Link href={subItem.url}>{subItem.title}</Link>
+                        </NavigationMenuLink>
+                      ))}
+                    </ul>
+                  </NavigationMenuContent>
+                </>
+              ) : (
+                <Link href={item.url}>{item?.title}</Link>
+              )}
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu> */}
     </nav>
   );
 }
