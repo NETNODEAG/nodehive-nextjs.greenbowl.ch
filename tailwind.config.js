7ded6de021bb3f2e7ad67eeb24c11fdb65ee91ec
Natalia Justicia <nataliajusticia@gmail.com>
2024-07-15 08:00:29 +0200

    Install shadcn/ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,20 @@ module.exports = {
       borderWidth: {
         3: '3px',
       },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
       // Customize typography for prose to match the theme colors/sizes/etc.
       // By default, prose uses a custom font-size, line-height, and color.
       typography: ({ theme }) => ({
@@ -80,5 +94,6 @@ module.exports = {
     require('@tailwindcss/forms')({
       strategy: 'base', // only generate global styles
     }),
+    require('tailwindcss-animate'),
   ],
 };
